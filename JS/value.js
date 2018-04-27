5134bb6590ec39308d528a36481de1619ed34cee
Jatin345Anand <31433847+Jatin345Anand@users.noreply.github.com>
2018-04-27 13:10:17 +0530

    AngularJSSPA
    MVC

---
+++ JS/value.js
@@ -0,0 +1,4 @@
+// app.constant("LocalUrl","http://localhost:5999/items");
+app.constant("CrudUrl","http://localhost:5000/mobiles");
+app.constant("UserUrl","http://localhost:5001/users");
+app.constant("AdminUrl","http://localhost:5002/admins");
